dfff1e5d50f54ed704488ca8572c847ae6e6641e
Rauulito <7rnavarro7@gmail.com>
2021-11-07 18:09:27 +0100

    realizado

--- Juego de las piedras.py
+++ Juego de las piedras.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
     t = int(input().strip())
     for t_itr in range(t):
-    n = int(input().strip())
+        n = int(input().strip())
     result = gameOfStones(n)
     fptr.write(result + '\n')
 fptr.close()
