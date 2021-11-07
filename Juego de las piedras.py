a699bd9eac3c30cc369d5f14f31ccf1712f7f1d7
Rauulito <7rnavarro7@gmail.com>
2021-11-07 18:40:43 +0100

    realizado

--- Juego de las piedras.py
+++ Juego de las piedras.py
@@ -14,7 +14,7 @@ def jugadaPerfecta(n):
     modulo=n%7  #Esto es asi porque lo sacamos del enunciado, esto de modulo 7 quiere decir que si tienes el numero 9 se comportara igaul que el numero 2
     piedrasExtraidas=0 #Unicamente pueden extraerse de 2,3 y 5
     if(modulo==2 or modulo==3):
-        piedrasExtraidas=2 
+        piedrasExtraidas=2
     if(modulo==4):
         piedrasExtraidas=3
     if(modulo==5 or modulo==6):
