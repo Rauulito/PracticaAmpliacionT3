7f170837b2ebe75296dcfec9d06decc840c6b588
Rauulito <7rnavarro7@gmail.com>
2021-11-07 18:34:42 +0100

    realizado

--- Juego de las piedras.py
+++ Juego de las piedras.py
@@ -10,8 +10,21 @@ import sys
 # The function accepts INTEGER n as parameter.
 
 #Creameremos un metodo para ver cual es la jugada buena y asi poder ver rapidamente si el gandor es P1 o P2
+def jugadaPerfecta(n):
+    modulo=n%7  #Esto es asi porque lo sacamos del enunciado, esto de modulo 7 quiere decir que si tienes el numero 9 se comportara igaul que el numero 2
+    piedrasExtraidas=0 #Unicamente pueden extraerse de 2,3 y 5
+    if(modulo==2 or modulo==3):
+        piedrasExtraidas=2 
+    if(modulo==4):
+        piedrasExtraidas=3
+    if(modulo==5 or modulo==6):
+        piedrasExtraidas=5
+    return piedrasExtraidas
 def gameOfStones(n):
-# Write your code here
+    if(jugadaPerfecta(n) != 0):
+        print("P1 es el ganador")
+    else:
+        print("P2 es el ganador")
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
