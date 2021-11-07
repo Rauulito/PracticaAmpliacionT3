bb3fcf5293a3b16dc02602ac413e84d4bd9761a3
Rauulito <7rnavarro7@gmail.com>
2021-11-07 13:38:17 +0100

    realizado

---
+++ La escalera.py
@@ -0,0 +1,22 @@
+import math
+import os
+import random
+import re
+import sys
+import numpy as np
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+def staircase(n):
+# Write your code here
+    for i in range(n):
+        i=i+1
+        print(i*'#')
+#Lo probamos
+staircase(6)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    staircase(n)
